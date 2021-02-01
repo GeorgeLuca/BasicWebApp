6b1f4cb6a805d5805fe6e63ea9aec04d42dfc82b
George-Luca  Florescu <gf1917@ic.ac.uk>
2021-02-01 15:40:13 +0200

    Make tests pass

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("shakespear")) {
+            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
+        }
         return "";
     }
 }

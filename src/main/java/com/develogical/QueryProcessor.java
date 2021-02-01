d96f27acca37f523a409da815861f1af93b44ca8
George-Luca  Florescu <gf1917@ic.ac.uk>
2021-02-01 16:42:58 +0200

    Add new query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -13,6 +13,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("meme")) {
+            return "Some dope meme ";
+        }
         return "";
     }
 }
